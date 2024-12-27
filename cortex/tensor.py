67a2b2d3d26750cc206199f1b63c831503350fc1
Shashwat Pandey <shashwatpandeym.v@gmail.com>
2024-12-26 19:03:44 -0700

    Release 0.0.0 cortex

--- cortex/tensor.py
+++ cortex/tensor.py
@@ -1,5 +1,8 @@
-import cupy as cp
 import numpy as np
+
+from .autograd import AutogradContext
+from .device import DeviceManager
+from .operations import TensorOperations
 
 
 class Tensor:
@@ -11,446 +14,84 @@ class Tensor:
         gradient=None,
         parents=None,
         operation="",
+        requires_grad=False,
     ):
         self.device = device
+        self.device_manager = DeviceManager(device)
         self.dtype = dtype
-        self.data = self._init_data(data)
+        self.data = self.device_manager.init_data(data, dtype)
         self.shape = self.data.shape
-        zeros = self._zeros(self.data.shape)
-        self.gradient = gradient if gradient is not None else self._init_data(zeros)
+        self.gradient = (
+            gradient if gradient is not None else self.device_manager.zeros(self.shape)
+        )
         self.parents = parents if parents is not None else []
         self.operation = operation
+        self.requires_grad = requires_grad
 
-    def _get_array_module(self):
-        return cp if self.device == "gpu" else np
-
-    def _log(self, x):
-        xp = self._get_array_module()
-        if (x <= 0).any():
-            raise ValueError("Log of negative number encountered")
-        return xp.log(x)
-
-    def _power(self, x, power):
-        xp = self._get_array_module()
-        return xp.power(x, power)
-
-    def _zeros(self, shape):
-        xp = self._get_array_module()
-        return xp.zeros(shape)
-
-    def _ones(self, shape):
-        xp = self._get_array_module()
-        return xp.ones(shape)
-
-    def _init_data(self, data, dtype=None):
-        xp = self._get_array_module()
-        if isinstance(data, (list, tuple)):
-            data = xp.array(data, dtype=self.dtype)
-        elif isinstance(data, (int, float, np.number)):
-            data = xp.array([data], dtype=self.dtype)
-        elif not isinstance(data, (xp.ndarray)):
-            raise TypeError(f"Unsupported data type: {type(data)}")
-        return data
-
-    def _check_compatibility(self, other):
-        if not isinstance(other, Tensor):
-            other = Tensor(other, device=self.device, dtype=self.dtype)
-        if self.device != other.device:
-            raise ValueError(
-                f"Cannot operate on different device types {self.device} and {other.device}"
-            )
-        if self.dtype != other.dtype:
-            raise ValueError(
-                f"Cannot operate on different dtypes {self.dtype} and {other.dtype}"
-            )
-        return other
+        self.ops = TensorOperations(self)
+        self.autograd = AutogradContext(self)
 
     def __repr__(self):
         return f"Tensor(\n{self.data})"
 
+    # Arithmetic operations
     def __add__(self, other):
-        other = self._check_compatibility(other)
-        broadcast_shape = self._broadcast_shape(self.shape, other.shape)
-        return Tensor(
-            self.data + other.data,
-            device=self.device,
-            dtype=self.dtype,
-            gradient=self._zeros(broadcast_shape),
-            parents=[self, other],
-            operation="+",
-        )
+        return self.ops.add(other)
 
     def __radd__(self, other):
-        other = self._check_compatibility(other)
-        broadcast_shape = self._broadcast_shape(self.shape, other.shape)
-        return Tensor(
-            self.data + other.data,
-            device=self.device,
-            dtype=self.dtype,
-            gradient=self._zeros(broadcast_shape),
-            parents=[self, other],
-            operation="+",
-        )
+        return self.ops.radd(other)
 
     def __mul__(self, other):
-        other = self._check_compatibility(other)
-        broadcast_shape = self._broadcast_shape(self.shape, other.shape)
-        return Tensor(
-            self.data * other.data,
-            device=self.device,
-            dtype=self.dtype,
-            gradient=self._zeros(broadcast_shape),
-            parents=[self, other],
-            operation="*",
-        )
+        return self.ops.multiply(other)
 
     def __rmul__(self, other):
-        other = self._check_compatibility(other)
-        broadcast_shape = self._broadcast_shape(self.shape, other.shape)
-        return Tensor(
-            self.data * other.data,
-            device=self.device,
-            dtype=self.dtype,
-            gradient=self._zeros(broadcast_shape),
-            parents=[self, other],
-            operation="*",
-        )
+        return self.ops.rmultiply(other)
 
     def __sub__(self, other):
-        other = self._check_compatibility(other)
-        broadcast_shape = self._broadcast_shape(self.shape, other.shape)
-        return Tensor(
-            self.data - other.data,
-            device=self.device,
-            dtype=self.dtype,
-            gradient=self._zeros(broadcast_shape),
-            parents=[self, other],
-            operation="-",
-        )
+        return self.ops.subtract(other)
 
     def __rsub__(self, other):
-        other = self._check_compatibility(other)
-        broadcast_shape = self._broadcast_shape(self.shape, other.shape)
-        return Tensor(
-            other.data - self.data,
-            device=self.device,
-            dtype=self.dtype,
-            gradient=self._zeros(broadcast_shape),
-            parents=[self, other],
-            operation="-",
-        )
+        return self.ops.rsubtract(other)
 
     def __truediv__(self, other):
-        other = self._check_compatibility(other)
-        if (other.data == 0).any():
-            raise ValueError("Division by zero encountered")
-        broadcast_shape = self._broadcast_shape(self.shape, other.shape)
-        return Tensor(
-            self.data / other.data,
-            device=self.device,
-            dtype=self.dtype,
-            gradient=self._zeros(broadcast_shape),
-            parents=[self, other],
-            operation="/",
-        )
+        return self.ops.divide(other)
 
     def __rtruediv__(self, other):
-        other = self._check_compatibility(other)
-        broadcast_shape = self._broadcast_shape(self.shape, other.shape)
-        return Tensor(
-            other.data / self.data,
-            device=self.device,
-            dtype=self.dtype,
-            gradient=self._zeros(broadcast_shape),
-            parents=[self, other],
-            operation="/",
-        )
+        return self.ops.rdivide(other)
 
     def __pow__(self, other):
-        other = self._check_compatibility(other)
-        if (self.data <= 0).any():
-            raise ValueError(
-                "Cannot compute power of negative numbers - will cause issues in backward pass"
-            )
-        broadcast_shape = self._broadcast_shape(self.shape, other.shape)
-        return Tensor(
-            self.data**other.data,
-            device=self.device,
-            dtype=self.dtype,
-            gradient=self._zeros(broadcast_shape),
-            parents=[self, other],
-            operation="**",
-        )
+        return self.ops.power(other)
 
     def __rpow__(self, other):
-        other = self._check_compatibility(other)
-        broadcast_shape = self._broadcast_shape(self.shape, other.shape)
-        return Tensor(
-            other.data**self.data,
-            device=self.device,
-            dtype=self.dtype,
-            gradient=self._zeros(broadcast_shape),
-            parents=[other, self],
-            operation="**",
-        )
+        return self.ops.rpower(other)
 
-    def sum(self):
-        xp = self._get_array_module()
-        return Tensor(
-            xp.sum(self.data),
-            device=self.device,
-            dtype=self.dtype,
-            parents=[self],
-            operation="sum",
-        )
-
-    def mean(self):
-        xp = self._get_array_module()
-        return Tensor(
-            xp.mean(self.data),
-            device=self.device,
-            dtype=self.dtype,
-            parents=[self],
-            operation="mean",
-        )
-
+    # Neural network operations
     def relu(self):
-        xp = self._get_array_module()
-        output = xp.maximum(0, self.data)
-
-        return Tensor(
-            output,
-            device=self.device,
-            dtype=self.dtype,
-            parents=[self],
-            operation="relu",
-            gradient=self._zeros(output.shape),
-        )
+        return self.ops.relu()
 
     def tanh(self):
-        xp = self._get_array_module()
-        return Tensor(
-            xp.tanh(self.data),
-            device=self.device,
-            dtype=self.dtype,
-            parents=[self],
-            operation="tanh",
-        )
+        return self.ops.tanh()
 
-    def zero_grad(self):
-        zeros = self._zeros(self.data.shape)
-        self.gradient = self._init_data(zeros)
+    # Tensor operations
+    def sum(self):
+        return self.ops.sum()
 
-    def _broadcast_shape(self, shape1, shape2):
-        xp = self._get_array_module()
-        s1 = xp.array(shape1)
-        s2 = xp.array(shape2)
-
-        if len(s1) < len(s2):
-            s1 = xp.pad(s1, (len(s2) - len(s1), 0), constant_values=1)
-        elif len(s2) < len(s1):
-            s2 = xp.pad(s2, (len(s1) - len(s2), 0), constant_values=1)
-
-        if not all((s1 == s2) | (s1 == 1) | (s2 == 1)):
-            raise ValueError(f"Shapes {shape1} and {shape2} are not broadcastable")
-
-        return tuple(xp.maximum(s1, s2))
+    def mean(self):
+        return self.ops.mean()
 
     def reshape(self, new_shape):
-        xp = self._get_array_module()
-        return Tensor(
-            xp.reshape(self.data, new_shape),
-            device=self.device,
-            dtype=self.dtype,
-            parents=[self],
-            operation="reshape",
-        )
+        return self.ops.reshape(new_shape)
 
     def transpose(self, *axes):
-        xp = self._get_array_module()
-        axes = axes if axes else None
-        return Tensor(
-            xp.transpose(self.data, axes),
-            device=self.device,
-            dtype=self.dtype,
-            parents=[self],
-            operation=("transpose", axes),
-        )
+        return self.ops.transpose(*axes)
 
-    def _reduce_gradient(self, gradient, from_shape, to_shape):
-        xp = self._get_array_module()
+    # Gradient operations
+    def zero_grad(self):
+        self.autograd.zero_grad()
 
-        if not from_shape:
-            from_shape = (1,)
-        if not to_shape:
-            to_shape = (1,)
+    def backward(self, gradient=None):
+        return self.autograd.backward(gradient)
 
-        if len(from_shape) > len(to_shape):
-            sum_axis = tuple(range(len(from_shape) - len(to_shape)))
-            gradient = xp.sum(gradient, axis=sum_axis)
-
-        elif len(from_shape) == len(to_shape):
-            sum_axis = []
-            for i, (f, t) in enumerate(zip(from_shape, to_shape)):
-                if f != t and t == 1:
-                    sum_axis.append(i)
-            if sum_axis:
-                gradient = xp.sum(gradient, axis=tuple(sum_axis), keepdims=True)
-
-        return gradient
-
-    def backward(self):
-        topo_order = self.build_graph()
-
-        xp = self._get_array_module()
-        if self.data.shape == (1,) or self.data.shape == ():
-            if xp.array_equal(self.gradient, self._zeros(self.data.shape)):
-                self.gradient = self._ones(self.data.shape)
-
-        for node in reversed(topo_order):
-            assert (
-                node.gradient.shape == node.data.shape
-            ), f"Gradient shape {node.gradient.shape} doesn't match data shape {node.data.shape}"
-            if node.operation == "+":
-                node.parents[0].gradient += self._reduce_gradient(
-                    node.gradient, node.shape, node.parents[0].shape
-                )
-                node.parents[1].gradient += self._reduce_gradient(
-                    node.gradient, node.shape, node.parents[1].shape
-                )
-
-            if node.operation == "*":
-                node.parents[0].gradient += self._reduce_gradient(
-                    node.parents[1].data * node.gradient,
-                    node.shape,
-                    node.parents[0].shape,
-                )
-                node.parents[1].gradient += self._reduce_gradient(
-                    node.parents[0].data * node.gradient,
-                    node.shape,
-                    node.parents[1].shape,
-                )
-
-            if node.operation == "-":
-                node.parents[0].gradient += self._reduce_gradient(
-                    node.gradient, node.shape, node.parents[0].shape
-                )
-                node.parents[1].gradient += self._reduce_gradient(
-                    -node.gradient, node.shape, node.parents[1].shape
-                )
-
-            if node.operation == "/":
-                node.parents[0].gradient += self._reduce_gradient(
-                    (1 / node.parents[1].data) * node.gradient,
-                    node.shape,
-                    node.parents[0].shape,
-                )
-                node.parents[1].gradient += self._reduce_gradient(
-                    (-node.parents[0].data / self._power(node.parents[1].data, 2))
-                    * node.gradient,
-                    node.shape,
-                    node.parents[1].shape,
-                )
-
-            if node.operation == "**":
-                eps = 1e-7
-                node.parents[0].gradient += self._reduce_gradient(
-                    node.parents[1].data
-                    * (
-                        self._power(
-                            node.parents[0].data + eps, node.parents[1].data - 1
-                        )
-                    )
-                    * node.gradient,
-                    node.shape,
-                    node.parents[0].shape,
-                )
-                node.parents[1].gradient += self._reduce_gradient(
-                    (self._power(node.parents[0].data, node.parents[1].data))
-                    * self._log(node.parents[0].data)
-                    * node.gradient,
-                    node.shape,
-                    node.parents[1].shape,
-                )
-            if node.operation == "relu":
-                xp = self._get_array_module()
-                mask = (node.parents[0].data > 0).astype(self.dtype)
-                grad = node.gradient * mask
-                node.parents[0].gradient += self._reduce_gradient(
-                    grad, node.shape, node.parents[0].shape
-                )
-
-            if node.operation == "tanh":
-                node.parents[0].gradient += self._reduce_gradient(
-                    (1 - self._power(node.data, 2)) * node.gradient,
-                    node.shape,
-                    node.parents[0].shape,
-                )
-            if node.operation == "sum":
-                node.parents[0].gradient += self._reduce_gradient(
-                    node.gradient * self._ones(node.parents[0].shape),
-                    node.shape,
-                    node.parents[0].shape,
-                )
-
-            if node.operation == "mean":
-                size = float(np.prod(node.parents[0].shape))
-                node.parents[0].gradient += self._reduce_gradient(
-                    node.gradient * self._ones(node.parents[0].shape) / size,
-                    node.shape,
-                    node.parents[0].shape,
-                )
-
-            if node.operation == "reshape":
-                node.parents[0].gradient += self._reduce_gradient(
-                    xp.reshape(node.gradient, node.parents[0].shape),
-                    node.shape,
-                    node.parents[0].shape,
-                )
-
-            if isinstance(node.operation, tuple) and node.operation[0] == "transpose":
-                _, axes = node.operation
-                node.parents[0].gradient += self._reduce_gradient(
-                    xp.transpose(node.gradient, axes), node.shape, node.parents[0].shape
-                )
-
-    def build_graph(self, visited_tensors=None, topo_order=None):
-        if visited_tensors is None:
-            visited_tensors = set()
-        if topo_order is None:
-            topo_order = []
-
-        visited_tensors.add(self)
-        for parent in self.parents:
-            if parent not in visited_tensors:
-                parent.build_graph(visited_tensors, topo_order)
-
-        topo_order.append(self)
-
-        return topo_order
-
+    # Device management
     def to(self, device):
-        if device == self.device:
-            return self
-
-        if device == "cpu":
-            new_data = cp.asnumpy(self.data) if self.device == "gpu" else self.data
-            new_gradient = (
-                cp.asnumpy(self.gradient) if self.device == "gpu" else self.gradient
-            )
-        elif device == "gpu":
-            new_data = cp.array(self.data) if self.device == "cpu" else self.data
-            new_gradient = (
-                cp.array(self.gradient) if self.device == "cpu" else self.gradient
-            )
-        else:
-            raise ValueError(f"Unknown device: {device}")
-
-        return Tensor(
-            new_data,
-            device=device,
-            dtype=self.dtype,
-            gradient=new_gradient,
-            parents=self.parents,
-            operation=self.operation,
-        )
+        return self.device_manager.transfer_to(device, self)
--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -1,106 +1,45 @@
 import unittest
 
-from cortex.tensor import Tensor
+import numpy as np
+
+from cortex import Tensor
 
 
-class TestTensor(unittest.TestCase):
-    """
-    Test suite for the Tensor class implementation.
-    Verifies core functionality including arithmetic operations,
-    gradient computation, and reduction operations.
-    """
+class TestTensorBasics(unittest.TestCase):
+    def setUp(self):
+        self.data = [1, 2, 3, 4]
+        self.tensor = Tensor(self.data)
 
-    def test_basic_operations(self):
-        """
-        Validates fundamental arithmetic operations on Tensor objects.
-        Ensures proper implementation of addition, multiplication,
-        subtraction, division, and power operations.
-        """
-        x = Tensor([2.0])
-        y = Tensor([3.0])
+    def test_initialization(self):
+        """Test tensor initialization with different data types"""
+        # List initialization
+        t1 = Tensor([1, 2, 3])
+        self.assertEqual(t1.shape, (3,))
 
-        z = x + y
-        self.assertEqual(z.data.tolist(), [5.0])
+        # Scalar initialization
+        t2 = Tensor(5)
+        self.assertEqual(t2.shape, (1,))
 
-        z = x * y
-        self.assertEqual(z.data.tolist(), [6.0])
+        # Numpy array initialization
+        t3 = Tensor(np.array([1, 2, 3]))
+        self.assertEqual(t3.shape, (3,))
 
-        z = y - x
-        self.assertEqual(z.data.tolist(), [1.0])
+    def test_device_placement(self):
+        """Test tensor device placement"""
+        cpu_tensor = Tensor([1, 2, 3], device="cpu")
+        self.assertEqual(cpu_tensor.device, "cpu")
 
-        z = y / x
-        self.assertEqual(z.data.tolist(), [1.5])
+        # Only test GPU if CUDA is available
+        try:
+            gpu_tensor = Tensor([1, 2, 3], device="gpu")
+            self.assertEqual(gpu_tensor.device, "gpu")
+        except:
+            pass
 
-        z = x ** Tensor([2.0])
-        self.assertEqual(z.data.tolist(), [4.0])
+    def test_invalid_initialization(self):
+        """Test invalid tensor initializations"""
+        with self.assertRaises(TypeError):
+            Tensor("invalid data")
 
-    def test_more_complex_gradients(self):
-        """
-        Tests gradient computation for a complex composite function:
-        f(w,x,y,z) = (w * x - y) / (z ** 2) + y * x
-
-        This test validates the correct implementation of the chain rule
-        and backpropagation through multiple arithmetic operations.
-
-        The gradients are verified against manually calculated derivatives:
-        ∂f/∂w = x/(z^2)
-        ∂f/∂x = w/(z^2) + y
-        ∂f/∂y = -1/(z^2) + x
-        ∂f/∂z = -2(w*x - y)/(z^3)
-        """
-        w = Tensor([2.0])
-        x = Tensor([3.0])
-        y = Tensor([4.0])
-        z = Tensor([2.0])
-
-        a = w * x
-        b = y
-        c = a - b
-        d = z**2
-        e = c / d
-        f = y * x
-        out = e + f
-
-        out.backward()
-
-        self.assertAlmostEqual(w.gradient.item(), 3 / 4)
-        self.assertAlmostEqual(x.gradient.item(), 4.5)
-        self.assertAlmostEqual(y.gradient.item(), 2.75)
-        self.assertAlmostEqual(z.gradient.item(), -0.5)
-
-    def test_reduction_operations(self):
-        """
-        Validates reduction operations (sum and mean) and their gradients.
-
-        Sum operation should propagate gradient of 1.0 to all input elements.
-        Mean operation should propagate gradient of 1/n to all input elements,
-        where n is the number of elements in the input tensor.
-        """
-        x = Tensor([1.0, 2.0, 3.0, 4.0])
-
-        y = x.sum()
-        self.assertEqual(y.data.item(), 10.0)
-        y.backward()
-        self.assertEqual(x.gradient.tolist(), [1.0, 1.0, 1.0, 1.0])
-
-        x = Tensor([1.0, 2.0, 3.0, 4.0])
-        y = x.mean()
-        self.assertEqual(y.data.item(), 2.5)
-        y.backward()
-        self.assertEqual(x.gradient.tolist(), [0.25, 0.25, 0.25, 0.25])
-
-    def test_error_handling(self):
-        """
-        Verifies proper error handling for invalid operations.
-        Currently tests division by zero protection, but should be
-        expanded to cover other edge cases and invalid operations
-        as the Tensor implementation grows.
-        """
-        x = Tensor([1.0])
-        y = Tensor([0.0])
         with self.assertRaises(ValueError):
-            x / y
-
-
-if __name__ == "__main__":
-    unittest.main()
+            Tensor([1, 2, 3], device="invalid_device")
